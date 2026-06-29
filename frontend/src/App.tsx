c8750c3c4afe0a293488681603f728f814981ff3
yarield <derexkcg@gmail.com>
2026-06-29 13:21:12 -0600

    COnfiguracon del stack tecnologuico

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,8 @@ function App() {
 
   return (
     <>
+    <h1 className="text-3xl font-bold text-blue-500">Hola Tailwind</h1>
+
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
